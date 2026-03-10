a6e42de1c3b734a0ce80f5c1fce29d1074c744d0
Deva <threpasty@gmail.com>
2026-03-10 09:02:08 +0800

    konversi suhu

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(486, 173);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Convert";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(282, 141);
+            label1.Name = "label1";
+            label1.Size = new Size(60, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Nilai Awal";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(282, 217);
+            label2.Name = "label2";
+            label2.Size = new Size(62, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Nilai Akhir";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(282, 172);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(171, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(282, 246);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(171, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Celcius", "Fahrenheit", "Kelvin", "Reamur" });
+            comboBox1.Location = new Point(130, 173);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 5;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Items.AddRange(new object[] { "Celcius", "Fahrenheit", "Kelvin", "Reamur" });
+            comboBox2.Location = new Point(130, 246);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(121, 23);
+            comboBox2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics.Eventing.Reader;
+
 namespace Modul3_103022400016
 {
     public partial class Form1 : Form
@@ -6,5 +8,85 @@ namespace Modul3_103022400016
         {
             InitializeComponent();
         }
+
+        static double celtoFah(double suhu) 
+        {
+            return (suhu * 9 / 5) + 32;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string suhuAwal = comboBox1.Text;
+            string suhuAkhir = comboBox2.Text;
+            string nilaiAwalText = textBox1.Text;
+
+
+            if (suhuAwal == null)
+            {
+                Console.WriteLine("pilih suhu pertama");
+            }
+            else if (suhuAkhir == null) 
+            {
+                Console.WriteLine("pilih suhu kedua");
+            }
+
+            if (suhuAwal.Equals("Celsius") && suhuAkhir.Equals("Fahrenheit"))
+            {
+                textBox2.Text = celtoFah(double.Parse(nilaiAwalText)).ToString();
+            }
+       
+            else if (suhuAwal == "Fahrenheit" && suhuAkhir == "Celsius")
+            {
+                double nilaiAwal = double.Parse(nilaiAwalText);
+                double nilaiAkhir = (nilaiAwal - 32) * 5 / 9;
+                textBox2.Text = nilaiAkhir.ToString();
+            }
+            else if (suhuAwal == "Celsius" && suhuAkhir == "Kelvin")
+            {
+                double nilaiAwal = double.Parse(nilaiAwalText);
+                double nilaiAkhir = nilaiAwal + 273;
+                textBox2.Text = nilaiAkhir.ToString();
+            }
+            else if (suhuAwal == "Kelvin" && suhuAkhir == "Celsius")
+            {
+                double nilaiAwal = double.Parse(nilaiAwalText);
+                double nilaiAkhir = nilaiAwal - 273;
+                textBox2.Text = nilaiAkhir.ToString();
+            }
+            else if (suhuAwal == "Fahrenheit" && suhuAkhir == "Kelvin")
+            {
+                double nilaiAwal = double.Parse(nilaiAwalText);
+                double nilaiAkhir = (nilaiAwal - 32) * 5 / 9 + 273;
+                textBox2.Text = nilaiAkhir.ToString();
+            }
+            else if (suhuAwal == "Kelvin" && suhuAkhir == "Fahrenheit")
+            {
+                double nilaiAwal = double.Parse(nilaiAwalText);
+                double nilaiAkhir = (nilaiAwal - 273) * 9 / 5 + 32;
+                textBox2.Text = nilaiAkhir.ToString();
+            }
+            else if (suhuAwal == "Celsius" && suhuAkhir == "Reamur")
+            {
+                double nilaiAwal = double.Parse(nilaiAwalText);
+                double nilaiAkhir = nilaiAwal * 4 / 5;
+                textBox2.Text = nilaiAkhir.ToString();
+            }
+            else if (suhuAwal == "Reamur" && suhuAkhir == "Celsius")
+            {
+                double nilaiAwal = double.Parse(nilaiAwalText);
+                double nilaiAkhir = nilaiAwal * 5 / 4;
+                textBox2.Text = nilaiAkhir.ToString();
+            }
+            else if (suhuAwal == suhuAkhir)
+            {
+                double suhu = double.Parse(nilaiAwalText);
+                textBox2.Text = suhu.ToString();
+            }
+             else
+            {
+                Console.WriteLine("pilih suhu yang valid");
+            }
+        }
+       
     }
 }
